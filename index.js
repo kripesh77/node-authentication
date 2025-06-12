e80febcbfeea96f7bcd9d225a7a49e4fa48c81fb
Kripesh <kg9773092+github.com>
2025-06-12 18:59:33 +0545

    Add auth middleware to check if user is logged in
    Unauthenticated users cannot access protected endpoints like '/me'.

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
-//JWT(JSON Web Token) is a stateless tokenization system
-//i.e., we do not need to store the token of the user anywhere
-//when server receives JWT token, server itself can decode the credentials used for encoding.
-//If server can decode the valid credential (initially used to encode), then server can ensure that the request is from valid user.
-
 const express = require("express");
 const jwt = require("jsonwebtoken");
 
-//We need this secret to encode and obtain our JWT token
-const JWT_SECRET = "blablabla";
+const JWT_SECRET = "blabla";
 
 const app = express();
 
 const users = [];
 
-//to parse request body into json
 app.use(express.json());
 
+function auth(req, res, next) {
+  const { token } = req.headers;
+
+  //If there is no token in user's header
+  if (token) {
+    //using built-in callback function of "jwt.verify" function
+    jwt.verify(token, JWT_SECRET, (err, decodedInfo) => {
+      // this decodedInfo and the one from above code is same
+      if (err) {
+        res.status(401).json({ message: "Token isn't valid" });
+      } else {
+        //modifying req so that routes using this middleware can have access to this "username" property as well
+        req.username = decodedInfo.username; //routes(i.e., /me) using this middleware also now has access to username property.
+        next();
+      }
+    });
+  } else {
+    res
+      .status(401)
+      .json({ message: "Please signin first to get access to this site" });
+    return;
+  }
+}
+
 app.post("/signup", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
+  const { username, password } = req.body;
 
   if (users.find((u) => u.username === username)) {
-    res.json({
-      message: "Username already exists",
-    });
+    res.json({ message: "User already exists" });
     return;
   }
 
@@ -33,60 +47,43 @@ app.post("/signup", (req, res) => {
   });
 
   res.json({ message: "You're signed in" });
-
-  console.log(users);
 });
 
 app.post("/signin", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
+  const { username, password } = req.body;
 
-  //"find" method returns first user having that username
   const user = users.find((u) => u.username === username);
-  //if there's no user, they've to first sign up
   if (!user) {
-    res.status(403).json({ message: "Please signup first" });
+    res.json({ message: "Please signup first" });
     return;
   }
 
-  //If we find user of that username but the password donot match, we do the following
   if (user.password !== password) {
-    res.status(403).json({ message: "Password donot match, please try again" });
-    return;
+    res.json({ message: "Password is incorrect" });
+  } else {
+    const token = jwt.sign({ username }, JWT_SECRET);
+    res.json({ token });
   }
-
-  //Now, if username is registered and password is also correct for that username
-  //then we assign a token to that user
-  const token = jwt.sign({ username }, JWT_SECRET); //converting username into a JWT using JWT_SECRET
-
-  //Now we don't need this line of code to store the token as we don't need to store it
-  //user.token = token;
-
-  res.json({ message: token });
-  console.log(users);
 });
 
-app.get("/me", (req, res) => {
-  //user still sends token, but this time it's JWT
-  const token = req.headers.token; //JWT
-
-  //in "/signin", just how we converted "username" object to JWT
-  //this line of code converts back the "username" object using JWT and JWT_SECRET
-  const decodedInformation = jwt.verify(token, JWT_SECRET); //gives: {username: "user's-name"}
-  const username = decodedInformation.username;
+app.get("/me", auth, (req, res) => {
+  const username = req.username;
 
   const user = users.find((u) => u.username === username);
 
   if (user) {
+    const { username, password } = user;
     res.json({
-      username: user.username,
-      password: user.password,
+      username,
+      password,
     });
   } else {
-    res.json({ message: "Not Authorized, Token Invalid" });
+    res.status(401).json({
+      message: "Not Authorized, Token invalid",
+    });
   }
 });
 
 app.listen(2000, () => {
-  console.log("Server is running in port 2000");
+  console.log("Server is listening at port 2000");
 });
